b09d560e1c9371bee539cf3d3929c00e2943793b
Vaithorat <vaibhavthorat141997@gmail.com>
2022-12-16 12:32:03 +0530

    added a POST request to add new subscriber details

--- src/app.js
+++ src/app.js
@@ -14,17 +14,33 @@ app.get("/subscribers", async (req, res) => {
   try {
     // get all the subscribers from the database and exclude the __v field
     const subscribers = await subscriberModel.find().select("-__v");
-    // return the subscribers to the client with a status code of 200
+    // return the subscribers with a status code of 200
     res.status(200).json(subscribers);
   } catch (err) {
-    // if there is an error, return a 500 status code with an error message
+    //incase of an error, return a status code of 500 with the following message
     res.status(500).json({ error: "database invalid" });
   }
 });
 
+//Sending POST request
+app.post("/subscribers", async (req, res) => {
+  //body to add details about the subscriber
+  const body = req.body;
+  //create a new subscriber and add it to database
+  const subscriber = await subscriberModel
+    .create(body)
+    .then((result) => {
+      //return the new result with status code of 200
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      //incase of an error, return a status code of 500 with the following message
+      res.status(500).json({ error: "Cannot add the element" });
+    });
+});
 //sending GET request
 app.get("/subscribers/:id", async (req, res) => {
-  // check if the id in the request params is a valid ObjectId
+  // check if the id is a valid ObjectId
   if (ObjectId.isValid(req.params.id)) {
     // get the subscriber with the given id from the database
     const subscribers = await subscriberModel
@@ -34,57 +50,57 @@ app.get("/subscribers/:id", async (req, res) => {
         res.status(200).json(result);
       })
       .catch((err) => {
-        // if there is an error, return a 500 status code with an error message
+        //incase of an error, return a status code of 500 with the following message
         res.status(500).json({
           error: `Error in fetching data with id ${ObjectId(req.params.id)}`,
         });
       });
   } else {
-    // if the id is not a valid ObjectId, return a 500 status code with an error message
+    // if the id is not a valid ObjectId, return a  status code of 500 with an error message
     res.status(500).json({ error: "invalid id" });
   }
 });
 
 //sending DELETE request
 app.delete("/subscribers/:id", async (req, res) => {
-    // check if the id in the request params is a valid ObjectId
-    if (ObjectId.isValid(req.params.id)) {
-      // delete the subscriber with the given id from the database
-      const subscribers = await subscriberModel
-        .find()
-        .deleteOne({ _id: ObjectId(req.params.id) })
-        .then((result) => {
-          // return the result of the delete operation to the client with a status code of 200
-          res.status(200).json(result);
-        })
-        .catch((err) => {
-          // if there is an error, return a 500 status code with an error message
-          res.status(500).json({ error: "Error in deleting" });
-        });
-    } else {
-      // if the id is not a valid ObjectId, return a 500 status code with an error message
-      res.status(500).json({ error: "invalid id" });
-    }
-  });
+  // check if the id in the request params is a valid ObjectId
+  if (ObjectId.isValid(req.params.id)) {
+    // delete the subscriber with the given id from the database
+    const subscribers = await subscriberModel
+      .find()
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        // return the result of the delete operation to the client with a status code of 200
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        //incase of an error, return a status code of 500 with the following message
+        res.status(500).json({ error: "Error in deleting" });
+      });
+  } else {
+    // if the id is not a valid ObjectId, return a  status code of 500 with an error message
+    res.status(500).json({ error: "invalid id" });
+  }
+});
 
-  app.patch("/subscribers/:id", async (req, res) => {
-    // check if the id in the request params is a valid ObjectId
-    if (ObjectId.isValid(req.params.id)) {
-      // update the subscriber with the given id in the database with the request body and return the updated document
-      const subscribers = await subscriberModel
-        .find()
-        .updateOne({ _id: ObjectId(req.params.id) }, req.body, { new: true })
-        .then((subscriber) => {
-          // return the updated subscriber document to the client with a status code of 200
-          res.status(200).json(subscriber);
-        })
-        .catch((err) => {
-          // if there is an error, return a 500 status code with an error message
-          res.status(500).json({ error: "Invalid request" });
-        });
-    } else {
-      // if the id is not a valid ObjectId, return a 500 status code with an error message
-      res.status(500).json({ error: "Invalid id" });
-    }
-  });
+app.patch("/subscribers/:id", async (req, res) => {
+  // check if the id in the request params is a valid ObjectId
+  if (ObjectId.isValid(req.params.id)) {
+    // update the subscriber with the given id in the database with the request body and return the updated document
+    const subscribers = await subscriberModel
+      .find()
+      .updateOne({ _id: ObjectId(req.params.id) }, req.body, { new: true })
+      .then((subscriber) => {
+        // return the updated subscriber document to the client with a status code of 200
+        res.status(200).json(subscriber);
+      })
+      .catch((err) => {
+        //incase of an error, return a status code of 500 with the following message
+        res.status(500).json({ error: "Invalid request" });
+      });
+  } else {
+    // if the id is not a valid ObjectId, return a  status code of 500 with an error message
+    res.status(500).json({ error: "Invalid id" });
+  }
+});
 module.exports = app;
