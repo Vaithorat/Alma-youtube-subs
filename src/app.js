149c143372ff8636f9e0884426a2cd14dc53252a
Vaithorat <vaibhavthorat141997@gmail.com>
2022-12-16 12:13:42 +0530

    Patch and delete via id has been added

--- src/app.js
+++ src/app.js
@@ -44,4 +44,47 @@ app.get("/subscribers/:id", async (req, res) => {
     res.status(500).json({ error: "invalid id" });
   }
 });
+
+//sending DELETE request
+app.delete("/subscribers/:id", async (req, res) => {
+    // check if the id in the request params is a valid ObjectId
+    if (ObjectId.isValid(req.params.id)) {
+      // delete the subscriber with the given id from the database
+      const subscribers = await subscriberModel
+        .find()
+        .deleteOne({ _id: ObjectId(req.params.id) })
+        .then((result) => {
+          // return the result of the delete operation to the client with a status code of 200
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          // if there is an error, return a 500 status code with an error message
+          res.status(500).json({ error: "Error in deleting" });
+        });
+    } else {
+      // if the id is not a valid ObjectId, return a 500 status code with an error message
+      res.status(500).json({ error: "invalid id" });
+    }
+  });
+
+  app.patch("/subscribers/:id", async (req, res) => {
+    // check if the id in the request params is a valid ObjectId
+    if (ObjectId.isValid(req.params.id)) {
+      // update the subscriber with the given id in the database with the request body and return the updated document
+      const subscribers = await subscriberModel
+        .find()
+        .updateOne({ _id: ObjectId(req.params.id) }, req.body, { new: true })
+        .then((subscriber) => {
+          // return the updated subscriber document to the client with a status code of 200
+          res.status(200).json(subscriber);
+        })
+        .catch((err) => {
+          // if there is an error, return a 500 status code with an error message
+          res.status(500).json({ error: "Invalid request" });
+        });
+    } else {
+      // if the id is not a valid ObjectId, return a 500 status code with an error message
+      res.status(500).json({ error: "Invalid id" });
+    }
+  });
 module.exports = app;
