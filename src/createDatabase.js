0beaf36b6aab90df589f81fcba021207d8244133
Vaithorat <vaibhavthorat141997@gmail.com>
2022-12-21 22:14:52 +0530

    database url changed

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -3,7 +3,7 @@ const subscriberModel = require('./models/subscribers')
 const data = require('./data')
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://127.0.0.1:27017/subscribers";
+const DATABASE_URL = "mongodb+srv://vaibhav:vaibhav141997@almabetter.qolyqo7.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
