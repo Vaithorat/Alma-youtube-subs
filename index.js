f55605d4ff7f83f97aa75a4d2eca74eae08dd212
Vaithorat <vaibhavthorat141997@gmail.com>
2022-12-22 00:44:09 +0530

    none

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const express = require('express')
-const app = require('./src/app')
-const mongoose = require('mongoose')
-const port = 3000
+const express = require("express");
+const app = require("./src/app");
+const mongoose = require("mongoose");
+const port = 3000;
+const app = express();
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // Connect to DATABASE
-const DATABASE_URL = "mongodb+srv://vaibhav:vaibhav141997@cluster0.4wefkqc.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-db.on('error', (err) => console.log(err))
-db.once('open', () => console.log('connected to database'))
-
-
+const DATABASE_URL =
+  "mongodb+srv://vaibhav:vaibhav141997@cluster0.4wefkqc.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("connected to database"));
 
 // Start Server
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
