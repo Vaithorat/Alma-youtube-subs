8f5d9720bd06ba32c6ba54672a8ca254a5c3a820
Vaithorat <vaibhavthorat141997@gmail.com>
2022-12-22 11:26:43 +0530

    db

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
-const subscriberModel = require('./models/subscriber');
-const data = require('./data');
+// const subscriberModel = require('./models/subscriber');
+// const data = require('./data');
 
-// Connect to DATABASE
-const DATABASE_URL = "mongodb+srv://vaibhav:vaibhav@cluster0.4wefkqc.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://vaibhav:vaibhav@cluster0.4wefkqc.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 const db = mongoose.connection;
-db.on('error', (err) => console.log(err));
-db.once('open', () => console.log('Database created...'));
 
-const refreshAll = async () => {
-    await subscriberModel.deleteMany({})
-    // console.log(connection)
-    await subscriberModel.insertMany(data)
-    await mongoose.disconnect();
-}
-refreshAll()
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
